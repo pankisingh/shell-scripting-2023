a3c4574b67a561464f85c713eadda1d1f53d0e55
Pankaj Singh <pankisingh070@gmail.com>
2023-01-25 00:57:36 +0530

    updated

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
 echo "hello i am Raj"
+
+#syntax for color echo -e "/e[color-codemMESSAGE"
+
+#syntac for new line echo -e "message\n\thello"
+
+
+echo -e "/e[31mMESSADE In RED \e0m"
