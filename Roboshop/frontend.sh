23df3255c124b7bb65a76d0241f85dba55d55ebb
Pankaj Singh <pankisingh070@gmail.com>
2023-01-28 00:11:45 +0530

    updated

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-
+LOG=/tmp/roboshop.log
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[m"
-yum install nginx -y
+yum install nginx -y >$LOG
 
 echo Enabling Nginx.
 systemctl enable nginx
