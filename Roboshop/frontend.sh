5a4a9c3d604c1f2265eacd5d8833d03d00193b3c
Pankaj Singh <pankisingh070@gmail.com>
2023-01-27 23:27:38 +0530

    updated

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+echo -e "Installing Nginx\t\t...\t\e[32mdone\e[m"
 yum install nginx -y
+
+echo Enabling Nginx.
 systemctl enable nginx
+
+echo Starting Nginx.
 systemctl start nginx
