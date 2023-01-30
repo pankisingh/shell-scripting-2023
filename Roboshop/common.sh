a554007742723be67c0c04ab5822c59658b5779e
Pankaj Singh <pankisingh070@gmail.com>
2023-01-30 22:55:53 +0530

    updated

--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -1,3 +1,114 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-yum install nginx -y
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+  echo -e "\e[31mYou should be root user / sudo user to run this script\e[0m"
+  exit 2
+fi
+
+LOG=/tmp/roboshop.log
+rm -f $LOG
+
+STAT_CHECK() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m fail\e[0m"
+    echo -e "\e[33m Check the log file for more details, Log File - $LOG\e[0m"
+    exit 1
+  fi
+}
+
+PRINT() {
+  echo -e "\n###########################\t$1\t###########################" &>>$LOG
+  echo -n -e "$1\t\t..."
+}
+
+ADD_APPLICATION_USER() {
+  PRINT "Add RoboShop Application User"
+  id roboshop &>>$LOG
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>$LOG
+  fi
+  STAT_CHECK $?
+}
+
+DOWNLOAD_APP_CODE() {
+  PRINT "Download Application Code"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  STAT_CHECK $?
+
+  PRINT "Extract Downloaded Code\t"
+  cd /home/roboshop && unzip -o  /tmp/${COMPONENT}.zip &>>$LOG  && rm -rf ${COMPONENT} && mv ${COMPONENT}-main ${COMPONENT}
+  STAT_CHECK $?
+}
+
+PERM_FIX() {
+  PRINT "Fix Application Permissions"
+  chown roboshop:roboshop /home/roboshop -R &>>$LOG
+  STAT_CHECK $?
+}
+
+SETUP_SYSTEMD() {
+  PRINT "Setup SystemD file\t"
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  STAT_CHECK $?
+
+  PRINT "Start ${COMPONENT} Service\t"
+  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  STAT_CHECK $?
+}
+
+NODEJS() {
+  PRINT "Install NodeJS\t\t"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  STAT_CHECK $?
+
+  ADD_APPLICATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Install NodeJS Dependencies"
+  cd /home/roboshop/${COMPONENT} && npm install --unsafe-perm &>>$LOG
+  STAT_CHECK $?
+
+  PERM_FIX
+  SETUP_SYSTEMD
+}
+
+JAVA() {
+  PRINT "Install Maven\t\t"
+  yum install maven -y &>>$LOG
+  STAT_CHECK $?
+
+  ADD_APPLICATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Compile Code\t\t"
+  cd /home/roboshop/${COMPONENT} && mvn clean package &>>$LOG &&  mv target/shipping-1.0.jar shipping.jar
+  STAT_CHECK $?
+
+  PERM_FIX
+  SETUP_SYSTEMD
+}
+
+PYTHON3() {
+  PRINT "Install Python3\t\t"
+  yum install python36 gcc python3-devel -y &>>$LOG
+  STAT_CHECK $?
+
+  ADD_APPLICATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Install Python Dependencies"
+  cd /home/roboshop/${COMPONENT} && pip3 install -r requirements.txt &>>$LOG
+  STAT_CHECK $?
+
+  PRINT "Update Service Configuration"
+  userID=$(id -u roboshop)
+  groupID=$(id -g roboshop)
+  sed -i -e "/uid/ c uid = ${userID}" -e "/gid/ c gid = ${groupID}" payment.ini &>>$LOG
+  STAT_CHECK $?
+
+  PERM_FIX
+  SETUP_SYSTEMD
+}
--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,10 +1,36 @@
-#!/usr/bin/bash
-LOG=/tmp/roboshop.log
-echo -e "Installing Nginx\t\t...\t\e[32mdone\e[m"
-yum install nginx -y >$LOG
+#!/bin/bash
 
-echo Enabling Nginx.
-systemctl enable nginx
+source common.sh
 
-echo Starting Nginx.
-systemctl start nginx
+PRINT "Installing Nginx\t"
+yum install nginx -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Download Frontend\t"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+
+PRINT "Remove Old HtDocs\t"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG  && rm -rf frontend-master static &>>$LOG
+STAT_CHECK $?
+
+PRINT "Copy RoboShop Config\t"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update RoboShop Config\t"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $?
+
+PRINT "Enabling Nginx\t\t"
+systemctl enable nginx  &>>$LOG
+STAT_CHECK $?
+
+PRINT "Starting Nginx\t\t"
+systemctl restart nginx  &>>$LOG
+STAT_CHECK $?
